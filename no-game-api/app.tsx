51febd7f284a69801adffa8fbe4cc13ad1bf770d
DESKTOP-HTMRD2N\Vincent <vincent.j.wilson317@gmail.com>
2024-03-02 15:23:46 -0300

    added some structure for api project blank models files

---
+++ no-game-api/app.tsx
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3000
+
+app.use(cors())
+
+// get lastest question set (question set with the most recent update
+// to a question in the collection)
+app.get('/api/questions', (req, res) => {
+  console.log('[GET] questions request made')
+  res.json([{
+      id: 1,
+      text: "i'm a mock question from the server?"
+    }]);
+})
+
+app.get('/api/questionsets/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(`[GET] /questionsets/${id} request made`)
+  res.json([{
+      id: 1,
+      text: "i'm a mock question from the server?"
+    }]);
+})
+
+app.get('/api/questionsets', (req, res) => {
+  console.log('[GET] questionsets request made')
+  res.json([{
+      id: 1,
+      text: "i'm a mock question from the server?"
+    }]);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
